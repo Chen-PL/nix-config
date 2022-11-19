83cb49b41eb5923f4afbc72eb1c6d0e139bda764
Chen <i@cuichen.cc>
2022-11-19 23:17:17 +0800

    lightdm settings

--- hosts/desktop/linux/common/nixos/xserver/default.nix
+++ hosts/desktop/linux/common/nixos/xserver/default.nix
@@ -10,25 +10,7 @@
 
     displayManager = {
       defaultSession = "none+xmonad";
-
-      lightdm = {
-        enable = true;
-        # greeters.mini = {
-        #   enable = true;
-        #   user = "chen";
-        #   extraConfig = ''
-        #     [greeter]
-        #     show-password-label = false
-        #     password-alignment = left
-        #     [greeter-theme]
-        #     background-color = "#2E3440"
-        #     window-color = "#4C566A"
-        #     border-color = "#4C566A"
-        #     text-color = "#5E81AC"
-        #     error-color = "#BF616A"
-        #   '';
-        # };
-      };
+      lightdm.enable = true;
     };
   };
 }
