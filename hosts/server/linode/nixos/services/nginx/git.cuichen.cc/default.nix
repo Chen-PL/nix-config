3bc16d14ba3ea5de77cf59ea94a346429ec0ef4f
Chen <i@cuichen.cc>
2022-11-20 20:47:47 +0800

    acme success

--- hosts/server/linode/nixos/services/nginx/git.cuichen.cc/default.nix
+++ hosts/server/linode/nixos/services/nginx/git.cuichen.cc/default.nix
@@ -14,41 +14,40 @@ in
     "git.cuichen.cc" = {
       forceSSL = true;
       enableACME = true;
-      root = ./html;
-      # root = "${pkgs.cgit}/cgit/";
+      root = "${pkgs.cgit}/cgit/";
 
-      # extraConfig = ''
-      #   try_files  $uri @cgit;
-      # '';
+      extraConfig = ''
+        try_files  $uri @cgit;
+      '';
 
-      # locations = {
-      #   "~ /.+/(info/refs|git-upload-pack)" = {
-      #     fastcgiParams = {
-      #       SCRIPT_FILENAME = "${pkgs.git}/bin/git-http-backend";
-      #       PATH_INFO = "$uri";
-      #       GIT_HTTP_EXPORT_ALL = "1";
-      #       GIT_PROJECT_ROOT = "/var/git";
-      #       HOME = "/var/git";
-      #     };
-      #     extraConfig = ''
-      #       include       ${pkgs.nginx}/conf/fastcgi_params;
-      #       fastcgi_pass  unix:/run/fcgiwrap.sock;
-      #     '';
-      #   };
-      #   "@cgit" = {
-      #     fastcgiParams = {
-      #       CGIT_CONFIG = cgitConfigFile;
-      #       SCRIPT_FILENAME = "${pkgs.cgit}/cgit/cgit.cgi";
-      #       PATH_INFO = "$uri";
-      #       QUERY_STRING = "$args";
-      #       HTTP_HOST = "$server_name";
-      #     };
-      #     extraConfig = ''
-      #       include       ${pkgs.nginx}/conf/fastcgi_params;
-      #       fastcgi_pass  unix:/run/fcgiwrap.sock;
-      #     '';
-      #   };
-      # };
+      locations = {
+        "~ /.+/(info/refs|git-upload-pack)" = {
+          fastcgiParams = {
+            SCRIPT_FILENAME = "${pkgs.git}/bin/git-http-backend";
+            PATH_INFO = "$uri";
+            GIT_HTTP_EXPORT_ALL = "1";
+            GIT_PROJECT_ROOT = "/var/git";
+            HOME = "/var/git";
+          };
+          extraConfig = ''
+            include       ${pkgs.nginx}/conf/fastcgi_params;
+            fastcgi_pass  unix:/run/fcgiwrap.sock;
+          '';
+        };
+        "@cgit" = {
+          fastcgiParams = {
+            CGIT_CONFIG = cgitConfigFile;
+            SCRIPT_FILENAME = "${pkgs.cgit}/cgit/cgit.cgi";
+            PATH_INFO = "$uri";
+            QUERY_STRING = "$args";
+            HTTP_HOST = "$server_name";
+          };
+          extraConfig = ''
+            include       ${pkgs.nginx}/conf/fastcgi_params;
+            fastcgi_pass  unix:/run/fcgiwrap.sock;
+          '';
+        };
+      };
     };
   };
 }
