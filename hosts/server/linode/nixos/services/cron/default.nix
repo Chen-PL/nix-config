4789a0409e448092ff1d812d3ad21a2f5d651ee9
Chen <i@cuichen.cc>
2022-11-20 23:55:53 +0800

    update cron

--- hosts/server/linode/nixos/services/cron/default.nix
+++ hosts/server/linode/nixos/services/cron/default.nix
@@ -2,7 +2,7 @@
   services.cron = {
     enable = true;
     systemCronJobs = [
-      ''*/1 * * * *    root    for d in /var/git/*.git ; do cd "$d"; git fetch; done''
+      ''*/1 * * * *    chen    for d in /var/git/*.git ; do cd "$d"; git fetch; done''
     ];
   };
 }
