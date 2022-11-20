605a3a2c1e38cf91b647e184045832e9f13fb082
Chen <i@cuichen.cc>
2022-11-20 13:58:32 +0800

    update ssh keys

--- hosts/common/home/programs/ssh/default.nix
+++ hosts/common/home/programs/ssh/default.nix
@@ -6,5 +6,12 @@
         identityFile = "~/.ssh/id_ed25519";
       };
     };
+
+    matchBlocks = {
+      linode = {
+        hostname = "server.cuichen.cc";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
   };
 }
