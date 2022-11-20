2fc6ed0251151263103d6a4ae9e995c5e2c36df2
Chen <i@cuichen.cc>
2022-11-20 23:15:57 +0800

    update virtual-root

--- hosts/server/linode/nixos/services/nginx/git.cuichen.cc/cgitrc.nix
+++ hosts/server/linode/nixos/services/nginx/git.cuichen.cc/cgitrc.nix
@@ -55,4 +55,6 @@
 
   include = ./cgitrepos;
 
+  virtual-root = "/";
+
 }
--- hosts/server/linode/nixos/services/nginx/git.cuichen.cc/default.nix
+++ hosts/server/linode/nixos/services/nginx/git.cuichen.cc/default.nix
@@ -20,19 +20,19 @@ in
       '';
 
       locations = {
-        # "~ /.+/(info/refs|git-upload-pack)" = {
-        #   fastcgiParams = {
-        #     SCRIPT_FILENAME = "${pkgs.git}/bin/git-http-backend";
-        #     PATH_INFO = "$uri";
-        #     GIT_HTTP_EXPORT_ALL = "1";
-        #     GIT_PROJECT_ROOT = "/var/git";
-        #     HOME = "/var/git";
-        #   };
-        #   extraConfig = ''
-        #     include       ${pkgs.nginx}/conf/fastcgi_params;
-        #     fastcgi_pass  unix:/run/fcgiwrap.sock;
-        #   '';
-        # };
+        "~ /.+/(info/refs|git-upload-pack)" = {
+          fastcgiParams = {
+            SCRIPT_FILENAME = "${pkgs.git}/bin/git-http-backend";
+            PATH_INFO = "$uri";
+            GIT_HTTP_EXPORT_ALL = "1";
+            GIT_PROJECT_ROOT = "/var/git";
+            HOME = "/var/git";
+          };
+          extraConfig = ''
+            include       ${pkgs.nginx}/conf/fastcgi_params;
+            fastcgi_pass  unix:${config.services.fcgiwrap.socketAddress};
+          '';
+        };
         "@cgit" = {
           fastcgiParams = {
             CGIT_CONFIG = configFile;
