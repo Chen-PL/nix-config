ce5674862a075205b34753b9f89b5e2f2609ea47
Chen <i@cuichen.cc>
2022-11-20 22:21:29 +0800

    repo configs

--- hosts/server/linode/nixos/services/nginx/git.cuichen.cc/default.nix
+++ hosts/server/linode/nixos/services/nginx/git.cuichen.cc/default.nix
@@ -6,11 +6,12 @@ let
   sepByNewline = lib.concatStringsSep "\n";
   repoToStr = repo: sepByNewline
     (mapAttrsToList (k: v: "repo.${k}=${toString v}") repo) ++ "\n";
+  secToStr = sec: sepByNewline (map repoToStr sec);
   cgitConfig = sepByNewline (
     mapAttrsToList (k: v: "${k}=${toString v}") (import ./cgitrc.nix));
-  reposConfig = sepByNewline (mapAttrsToList
-    (k: v: "section=${k}\n${map repoToStr v}")
-    (import ./cgitrepos.nix));
+  reposConfig = mapAttrsToList
+    (k: v: "section=${k}\n${secToStr v}\n")
+    (import ./cgitrepos.nix);
   configFile = pkgs.writeText "cgitrc" (cgitConfig ++ "\n" ++ reposConfig);
 in
 {
