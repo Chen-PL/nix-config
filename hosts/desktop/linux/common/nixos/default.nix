af5f89afbdead49459ee53e41b22cf7c694711a7
Chen <i@cuichen.cc>
2022-11-20 14:08:47 +0800

    removed rsa keys considering security

--- hosts/desktop/linux/common/nixos/default.nix
+++ hosts/desktop/linux/common/nixos/default.nix
@@ -9,7 +9,7 @@
   nix = {
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-    
+
     gc = {
       automatic = true;
       dates = "weekly";
@@ -56,8 +56,6 @@
       extraGroups = [ "audio" "docker" "networkmanager" "vboxusers" "wheel" ];
 
       openssh.authorizedKeys.keys = [
-        # Macbook
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDdOQtVDoRn3HoxYJwzYw3n3+LL97UKbYlicewbmWD0Hid1lq+abPBuOSUxLFDWyOC1Y8fTAJJQJEt+2a1ReVsQsriedj7i2dZqb0ydme8YsC+TEDV9HnhfjMk2Ee/Cju5ShAX/4zdjr1p+Y18VmmRVkAZYUOsAW3nYtEYu8OWZiWsHb5klyW6RnTdEGmfinO/QSx7UgQIbaRj0QjGu3JEOx6CubhGIhr14ReSlHT1ltpur2dyG9l3Ux17mlfwg5XeaVSt3rznBz/e3eQO1pyh+Tn9nTaC1g2im3Dymee8iF1+ARFcE7OBDKMGefRZnSTx1GUBZifMt9on9EBG61S+m5MFvlXL0/6OWcZhkda0HhyK8PyLe/x+b+1uXS7b4980YUpn0thtGRlunWPzEKKZqeSPtuCxm956WQOHypXT5g3r7M6wkiqAh3t0tu8mzVj7EtxJLpsRnPv+TgNd+2iAd7R/UQ3hRd3vwrBkvF4hm1zY5ZImCo52jbg0ZaqB09Fc= cuichen"
       ];
     };
   };
