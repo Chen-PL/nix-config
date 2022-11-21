429cd29c05d8955e7498a8436d9b5b336284c9ee
Chen <i@cuichen.cc>
2022-11-21 17:29:13 +0800

    location testing

--- hosts/server/linode/nixos/services/nginx/note.cuichen.cc/default.nix
+++ hosts/server/linode/nixos/services/nginx/note.cuichen.cc/default.nix
@@ -27,15 +27,17 @@ in
       forceSSL = true;
       enableACME = true;
 
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString researchWikiPort}";
-        proxyWebsockets = true; # needed if you need to use WebSocket
-        extraConfig =
-          # required when the target is also TLS server with multiple hosts
-          "proxy_ssl_server_name on;" +
-          # required when the server wants to use HTTP Authentication
-          "proxy_pass_header Authorization;"
-        ;
+      locations = {
+        "/research/" = {
+          proxyPass = "http://127.0.0.1:${toString researchWikiPort}/";
+          proxyWebsockets = true; # needed if you need to use WebSocket
+          extraConfig =
+            # required when the target is also TLS server with multiple hosts
+            "proxy_ssl_server_name on;" +
+            # required when the server wants to use HTTP Authentication
+            "proxy_pass_header Authorization;"
+          ;
+        };
       };
     };
   };
