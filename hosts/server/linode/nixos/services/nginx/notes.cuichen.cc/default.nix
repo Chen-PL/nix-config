5f863446d9245ea4227d065c161598a7ec77d0c3
Chen <i@cuichen.cc>
2022-11-21 16:57:17 +0800

    notes.cuichen.cc

--- hosts/server/linode/nixos/services/nginx/notes.cuichen.cc/default.nix
+++ hosts/server/linode/nixos/services/nginx/notes.cuichen.cc/default.nix
@@ -1,9 +1,48 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  listenParams = options: concatStrings (mapAttrsToList (n: v: " '${n}=${toString v}' ") options);
+  exe = "${pkgs.nodePackages.tiddlywiki}/lib/node_modules/.bin/tiddlywiki";
+  dataDir = name: "/var/lib/wiki/" + name;
+  servWiki = name: options: {
+    description = "TiddlyWiki nodejs server";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      Restart = "on-failure";
+      DynamicUser = true;
+      StateDirectory = "wiki/" + name;
+      ExecStartPre = "-${exe} ${dataDir name} --init server";
+      ExecStart = "${exe} ${dataDir name} --listen ${listenParams options}";
+    };
+  };
+  researchWikiPort = 8100;
+in
 {
   services.nginx.virtualHosts = {
     "note.cuichen.cc" = {
       forceSSL = true;
       enableACME = true;
-      root = ./html;
+
+      locations."/research/" = {
+        proxyPass = "http://127.0.0.1:${toString researchWikiPort}";
+        proxyWebsockets = true; # needed if you need to use WebSocket
+        extraConfig =
+          # required when the target is also TLS server with multiple hosts
+          "proxy_ssl_server_name on;" +
+          # required when the server wants to use HTTP Authentication
+          "proxy_pass_header Authorization;"
+        ;
+      };
     };
+  };
+
+  systemd.services.tiddlywiki-research = servWiki "research" {
+    credentials = "../credentials-research.csv";
+    port = researchWikiPort;
+    readers = "(authenticated)";
   };
 }
