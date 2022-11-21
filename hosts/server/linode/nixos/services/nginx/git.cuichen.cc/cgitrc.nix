f2a87522f300c97442bf595dd1122755edcf7e31
Chen <i@cuichen.cc>
2022-11-21 11:17:52 +0800

    update cgitrc

--- hosts/server/linode/nixos/services/nginx/git.cuichen.cc/cgitrc.nix
+++ hosts/server/linode/nixos/services/nginx/git.cuichen.cc/cgitrc.nix
@@ -1,49 +1,25 @@
 cgitPkg: ''
-# Enable caching of up to 1000 output entriess
-cache-size=1000
+virtual-root=/ # Required for Nginx
 
-# Specify some default clone urls using macro expansion
-clone-url=git://git.cuichen.cc/$CGIT_REPO_URL git@git.cuichen.cc:$CGIT_REPO_URL
-
-# Specify the css url
 css=/cgit.css
-
-# Show extra links for each repository on the index page
-enable-index-links=1
-
-# Enable ASCII art commit history graph on the log pages
-enable-commit-graph=1
-
-# Show number of affected files per commit on the log pages
-enable-log-filecount=1
-
-# Show number of added/removed lines per commit on the log pages
-enable-log-linecount=1
-
-# Add a cgit favicon
 favicon=/favicon.ico
-
-# Use a custom logo
-# logo=/img/mylogo.png
-
-# Enable statistics per week, month and quarter
-max-stats=quarter
-
-# Set the title and heading of the repository index page
+logo=
+footer=${./footer.html}
+source-filter=${cgitPkg}/lib/cgit/filters/syntax-highlighting.py
+about-filter=${cgitPkg}/lib/cgit/filters/about-formatting.sh
 root-title=Chen's Git Repositories
+root-desc=
+noplainemail=1
+robots=noindex, nofollow
+readme=:README.md
 
-# Set a subheading for the repository index page
-root-desc=My personal projects
+clone-prefix=https://git.cuichen.cc
 
-# Include some more info about example.com on the index page
-# root-readme=/var/www/htdocs/about.html
-
-# Allow download of tar.gz, tar.bz2 and zip-files
+# Enable snapshots
 snapshots=tar.gz tar.bz2 zip
 
-##
-## List of common mimetypes
-##
+# Disable owner index
+enable-index-owner=0
 
 mimetype.gif=image/gif
 mimetype.html=text/html
@@ -53,9 +29,11 @@ mimetype.pdf=application/pdf
 mimetype.png=image/png
 mimetype.svg=image/svg+xml
 
-source-filter=${cgitPkg}/lib/cgit/filters/syntax-highlighting.py
+# Caching
+cache-dynamic-ttl=60
+cache-static-ttl=44640
+cache-root-ttl=6
+cache-repo-ttl=120
 
-virtual-root=/
 include=${./cgitrepos}
-footer=${./footer.html}
 ''
