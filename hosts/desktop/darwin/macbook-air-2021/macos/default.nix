e82630ed5b08d05060b5d9ae75d7d4b3988bbe05
Chen <i@cuichen.cc>
2022-11-23 15:51:04 +0800

    disable auto gc for mac

--- hosts/desktop/darwin/macbook-air-2021/macos/default.nix
+++ hosts/desktop/darwin/macbook-air-2021/macos/default.nix
@@ -11,12 +11,6 @@
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-
     settings = {
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
