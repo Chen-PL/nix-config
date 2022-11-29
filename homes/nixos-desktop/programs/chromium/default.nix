fed71e0c5615f3ac923c222a0c9e235abe164c87
Chen <i@cuichen.cc>
2022-11-29 23:34:03 +0800

    firefox dmg

--- homes/nixos-desktop/programs/chromium/default.nix
+++ homes/nixos-desktop/programs/chromium/default.nix
@@ -1,6 +1,9 @@
+{ pkgs, ... }:
+
 {
   programs.chromium = {
     enable = true;
+    package = pkgs.chromium;
     extensions = [
       { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
       { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1Password
--- overlays/darwin/default.nix
+++ overlays/darwin/default.nix
@@ -17,5 +17,18 @@
         description = "Sophisticated text editor for code, markup and prose";
         homepage = "https://www.sublimetext.com/";
       };
+
+      firefox = installApplication rec {
+        name = "Firefox";
+        version = "107.0.1";
+        sourceRoot = "Firefox.app";
+        src = prev.fetchurl {
+          name = "Firefox-${version}.dmg";
+          url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/107.0.1/mac/zh-CN/Firefox%20${version}.dmg";
+          sha256 = "sha256-K9VLq8ayc2ToDEwQfOG0ACZmI3H1bDXxWyn967KUKxk=";
+        };
+        description = "The Firefox web browser";
+        homepage = "https://www.mozilla.org/en-US/firefox/";
+      };
     };
 }
