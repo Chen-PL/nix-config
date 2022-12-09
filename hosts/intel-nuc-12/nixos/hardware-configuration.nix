40d9c0ed1a71a0d162bfab8522c748dc7c279b07
Chen-PL <i@cuichen.cc>
2022-12-09 13:21:05 +0800

    Update hardware-configuration.nix

--- hosts/intel-nuc-12/nixos/hardware-configuration.nix
+++ hosts/intel-nuc-12/nixos/hardware-configuration.nix
@@ -16,17 +16,17 @@
   ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/29199ab8-28f0-46c5-81a0-301055e41c9e";
+    device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/D63B-93B4";
+    device = "/dev/disk/by-label/boot";
     fsType = "vfat";
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-uuid/0ec4710f-a7d8-4a18-887a-8e705863c916"; }
+    { device = "/dev/disk/by-label/swap"; }
   ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
