ba8de4c03004cacd4bce387a2c022d63c8d6372a
Chen <i@cuichen.cc>
2022-12-09 15:29:58 +0800

    move hidpi settings to home

--- homes/nixos-desktop/default.nix
+++ homes/nixos-desktop/default.nix
@@ -8,5 +8,12 @@
     ./themes
   ];
 
+  home.sessionVariables = {
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5";
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+    QT_SCALE_FACTOR = "1.5";
+  };
+
   fonts.fontconfig.enable = true;
 }
--- systems/nixos-desktop/xserver/default.nix
+++ systems/nixos-desktop/xserver/default.nix
@@ -19,11 +19,4 @@
   # HiDPI
   hardware.video.hidpi.enable = true;
   services.xserver.dpi = 180;
-
-  environment.variables = {
-    GDK_SCALE = "2";
-    GDK_DPI_SCALE = "0.5";
-    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
-    QT_SCALE_FACTOR = "2";
-  };
 }
